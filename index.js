6d8e826e97876f67697e97ef613aa5e1cebe0bd1
JB <jacobbunnellca@gmail.com>
2022-07-16 16:20:58 -0400

    .

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ const background = new Sprite({
     },
     imageSrc: './img/background.png'
 })
+
+const shop = new Sprite({
+    position: {
+       x: 600,
+       y: 130
+    },
+    imageSrc: './img/shop.png',
+    scale: 2.75
+})
+
 const player = new Fighter({
     position: {
         x: 0,
@@ -75,6 +85,7 @@ function animate() {
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     background.update()
+    shop.update()
     player.update()
     enemy.update()
 
