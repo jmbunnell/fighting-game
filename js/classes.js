ea0320c13eb319a8f4dc188ce7d51ba31fb484a5
JB <jacobbunnellca@gmail.com>
2022-07-17 12:49:46 -0400

    .

--- js/classes.js
+++ js/classes.js
@@ -108,13 +108,17 @@ class Fighter extends Sprite {
 
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
-
+        
+        // Gravity Function
         if (this.position.y + this.height + this.velocity.y >= canvas.height - 96) {
             this.velocity.y = 0
+            this.position.y = 330
         } else this.velocity.y += gravity
+
     }
 
     attack() {
+        this.switchSprite('attack1')
         this.isAttacking = true
         setTimeout(() => {
             this.isAttacking = false
@@ -122,6 +126,12 @@ class Fighter extends Sprite {
     }
     
     switchSprite(sprite) {
+        if (
+            this.image === this.sprites.attack1.image && 
+            this.framesCurrent < this.sprites.attack1.framesMax - 1
+        ) 
+            return
+
         switch (sprite) {
             case 'idle':
                 if (this.image !== this.sprites.idle.image) {
@@ -148,6 +158,13 @@ class Fighter extends Sprite {
                 if (this.image !== this.sprites.fall.image) {
                     this.image = this.sprites.fall.image
                     this.framesMax = this.sprites.fall.framesMax
+                    this.framesCurrent = 0
+                }
+                break
+            case 'attack1':
+                if (this.image !== this.sprites.attack1.image) {
+                    this.image = this.sprites.attack1.image
+                    this.framesMax = this.sprites.attack1.framesMax
                     this.framesCurrent = 0
                 }
                 break
