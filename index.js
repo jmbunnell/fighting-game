7c1490137177fd26793ba7bf92bc2cf6863281c3
JB <jacobbunnellca@gmail.com>
2022-07-17 13:31:24 -0400

    .

--- index.js
+++ index.js
@@ -67,6 +67,14 @@ const player = new Fighter({
             imageSrc: './img/samuraiMack/Attack1.png',
             framesMax: 6
         }
+    },
+    attackBox: {
+        offset: {
+            x: 100,
+            y: 50
+        },
+        width: 160,
+        height: 50
     }
 })
 
@@ -113,6 +121,14 @@ const enemy = new Fighter({
             imageSrc: './img/kenji/Attack1.png',
             framesMax: 4
         }
+    },
+    attackBox: {
+        offset: {
+            x: -170,
+            y: 50
+        },
+        width: 170,
+        height: 50
     }
 })
 
@@ -191,14 +207,20 @@ function animate() {
     // Detect for collision
     if (
         rectangularCollision({
-            rectangle1: enemy,
+            rectangle1: player,
             rectangle2: enemy
         }) &&
-        player.isAttacking
+        player.isAttacking && 
+        player.framesCurrent === 4
     ) {
         player.isAttacking = false
         enemy.health -= 20
         document.querySelector('#enemyHealth').style.width = enemy.health + '%'
+    }
+
+    // If player misses
+    if (player.isAttacking && player.framesCurrent === 4) {
+        player.isAttacking = false
     }
 
     if (
@@ -206,11 +228,17 @@ function animate() {
             rectangle1: enemy,
             rectangle2: player
         }) &&
-        enemy.isAttacking
+        enemy.isAttacking && 
+        enemy.framesCurrent === 2
     ) {
         enemy.isAttacking = false
         player.health -= 20
         document.querySelector('#playerHealth').style.width = player.health + '%'
+    }
+
+    // If enemy misses
+    if (enemy.isAttacking && enemy.framesCurrent === 2) {
+        enemy.isAttacking = false
     }
 
     //end game based on health
--- js/classes.js
+++ js/classes.js
@@ -60,7 +60,8 @@ class Fighter extends Sprite {
         scale = 1, 
         framesMax = 1,
         offset = { x: 0, y: 0 },
-        sprites 
+        sprites,
+        attackBox = { offset: {}, width: undefined, height: undefined } 
     }) {
         super({
             position,
@@ -80,9 +81,9 @@ class Fighter extends Sprite {
                 x: this.position.x,
                 y: this.position.y
             },
-            offset,
-            width: 100,
-            height: 50
+            offset: attackBox.offset,
+            width: attackBox.width,
+            height: attackBox.height
         }
         this.color = color
         this.isAttacking
@@ -103,8 +104,18 @@ class Fighter extends Sprite {
         this.draw()
         this.animateFrames()
 
+        // Attack boxes
         this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y
+        this.attackBox.position.y = this.position.y + this.attackBox.offset.y
+
+        // Draw attack box
+        /*
+        c.fillRect(
+            this.attackBox.position.x, 
+            this.attackBox.position.y, 
+            this.attackBox.width, 
+            this.attackBox.height
+        )*/
 
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
@@ -120,9 +131,6 @@ class Fighter extends Sprite {
     attack() {
         this.switchSprite('attack1')
         this.isAttacking = true
-        setTimeout(() => {
-            this.isAttacking = false
-        }, 100)
     }
     
     switchSprite(sprite) {
